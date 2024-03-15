75483b1e65bfb4b61d985c55aadb7d388073b49d
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-14 22:52:03 -0300

    Install shadcn ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,34 @@
-import { defineNuxtConfig } from "nuxt/config";
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  modules: ["@nuxtjs/tailwindcss", "shadcn-nuxt"],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: "",
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: "./components/ui",
+  },
+  app: {
+    head: {
+      link: [
+        {
+          rel: "preconnect",
+          href: "https://fonts.googleapis.com",
+        },
+        {
+          rel: "preconnect",
+          href: "https://fonts.gstatic.com",
+        },
+        {
+          href: "https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap",
+          rel: "stylesheet",
+        },
+      ],
+    },
+  },
 });
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    Hello world!
+    <Button>Click me</Button>
   </div>
 </template>
 
