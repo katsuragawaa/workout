75f9318bf8fe52b2c03c9bdb54f4df2c39505d16
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-16 11:48:20 -0300

    Create workout index page

---
+++ components/workout-card/WorkoutCard.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+import { ChevronRight } from "lucide-vue-next";
+
+const props = defineProps<{
+  title: string;
+  description: string;
+  id: number;
+}>();
+
+</script>
+
+<template>
+  <Card>
+    <CardHeader class="flex flex-row items-center justify-between gap-4">
+      <div class="space-y-1.5">
+        <CardTitle>{{ props.title }}</CardTitle>
+        <CardDescription>{{ props.description }}</CardDescription>
+      </div>
+
+      <Button as-child variant="outline" size="icon" class="min-w-10">
+        <NuxtLink :href="`/workout/${props.id}`">
+          <ChevronRight class="h-4 w-4" />
+        </NuxtLink>
+      </Button>
+    </CardHeader>
+  </Card>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,10 +1,32 @@
-<template>
-  <div>
-    <Button>Click me</Button>
-  </div>
-</template>
-
 <script lang="ts" setup>
+
 </script>
 
-<style></style>
+<template>
+  <div>
+    <header class="container flex max-w-4xl items-center justify-end pt-14">
+      <!-- <theme-toggle /> -->
+    </header>
+
+    <main class="container flex max-w-xl flex-col py-10">
+      <h1 class="pb-10 text-5xl font-extrabold">
+        Choose
+        <br />
+        Your
+        <br />
+        Workout
+      </h1>
+
+      <section class="flex flex-col gap-4">
+        <!-- map workouts -->
+        <WorkoutCard :id="1" title="Test workout" description="Lopem ipsum" />
+      </section>
+
+      <Button as-child>
+        <NuxtLink href="/workout/new" class="mt-10">
+          Novo Treino
+        </NuxtLink>
+      </Button>
+    </main>
+  </div>
+</template>
