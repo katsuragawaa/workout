68694955cbb5c710265fbb874a0456c0555c80bb
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-23 19:15:15 -0300

    Update ESLint configuration

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,7 @@
 module.exports = {
-  // ...
   parser: "vue-eslint-parser",
   parserOptions: {
     parser: "@typescript-eslint/parser",
   },
   extends: ["@nuxt/eslint-config", "plugin:prettier/recommended"],
-  // ...
 };
