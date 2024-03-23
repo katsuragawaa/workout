6f8bd22e85a912b58cb61d3ca965d8fe7aa9c717
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-23 19:34:08 -0300

    Reformat component

--- components/exercise-form-item/ExerciseFormItem.vue
+++ components/exercise-form-item/ExerciseFormItem.vue
@@ -28,7 +28,9 @@ const confirmDelete = () => {
   <div class="flex flex-col justify-center rounded-md border p-4">
     <div class="flex items-center justify-between gap-4">
       <div class="space-y-2">
-        <h3 class="text-lg font-medium leading-5">{{ props.name }}</h3>
+        <h3 class="text-lg font-medium leading-5">
+          {{ props.name }}
+        </h3>
         <div class="flex items-center text-xs">
           <div class="flex flex-row items-center gap-1">
             <Beef class="h-4 w-4" />
@@ -48,17 +50,27 @@ const confirmDelete = () => {
 
       <DropdownMenu>
         <DropdownMenuTrigger as-child>
-          <Button variant="ghost" size="icon" class="min-w-10">
+          <Button
+            variant="ghost"
+            size="icon"
+            class="min-w-10"
+          >
             <MoreVertical class="h-4 w-4" />
             <span class="sr-only">More</span>
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent>
-          <DropdownMenuItem @click="handleEdit" class="flex items-center gap-2">
+          <DropdownMenuItem
+            class="flex items-center gap-2"
+            @click="handleEdit"
+          >
             <Pencil class="h-3 w-3" />
             Editar
           </DropdownMenuItem>
-          <DropdownMenuItem @click="handleDelete" class="flex items-center gap-2">
+          <DropdownMenuItem
+            class="flex items-center gap-2"
+            @click="handleDelete"
+          >
             <Trash2 class="h-3 w-3" />
             Deletar
           </DropdownMenuItem>
