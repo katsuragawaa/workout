5a945efa5207a7a48da529667f37bbd28ccede54
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-23 18:34:08 -0300

    Add exercise form validation and submission logic

--- components/exercise-form-dialog/ExerciseFormDialog.vue
+++ components/exercise-form-dialog/ExerciseFormDialog.vue
@@ -1,9 +1,42 @@
 <script lang="ts" setup>
+import { muscles } from "@/lib/muscles";
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import * as z from "zod";
+
+const formSchema = toTypedSchema(
+  z.object({
+    id: z.string().uuid().optional(),
+    workoutId: z.string().uuid(),
+    name: z.string().min(2).max(50),
+    muscle: z.string().refine((value) => muscles.some((muscle) => muscle.value === value), {
+      message: "You must select a valid option",
+    }),
+    sets: z.number().min(1).max(5),
+    reps: z.number().min(1).max(30),
+  }),
+);
+
+const defaultExercise = {
+  id: "",
+  workoutId: "",
+  name: "",
+  muscle: muscles[0].value,
+  sets: 1,
+  reps: 1,
+};
+
+const form = useForm({
+  validationSchema: formSchema,
+  initialValues: defaultExercise,
+});
+
+const onSubmit = form.handleSubmit((values) => {
+  console.log('Form submitted!', values)
+})
 
 </script>
 
 <template>
-  <div>
-    hi
-  </div>
+  <div>hi</div>
 </template>
