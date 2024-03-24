74ec057778425b953151343a84c62f1a8d35768e
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-23 21:55:07 -0300

    Refactor mock-db.ts to use ulid for generating unique IDs

--- lib/mock-db.ts
+++ lib/mock-db.ts
@@ -1,9 +1,8 @@
 import type { Exercise, Workout } from "@/types";
+import { ulid } from "ulidx";
 
 const workouts: Workout[] = [];
 const exercises: Exercise[] = [];
-let lastWorkoutId = 0;
-let lastExerciseId = 0;
 
 const getWorkouts = () => {
   return workouts;
@@ -13,42 +12,42 @@ const getExercises = () => {
   return exercises;
 };
 
-const getExercisesByWorkout = (workoutId: number) => {
+const getExercisesByWorkout = (workoutId: string) => {
   return exercises.filter((exercise) => exercise.workoutId === workoutId);
 };
 
 const saveWorkout = (workout: Omit<Workout, "id">) => {
-  const newWorkout = { ...workout, id: ++lastWorkoutId };
+  const newWorkout = { ...workout, id: ulid() };
   workouts.push(newWorkout);
 };
 
 const saveExercise = (exercise: Omit<Exercise, "id">) => {
-  const newExercise = { ...exercise, id: ++lastExerciseId };
+  const newExercise = { ...exercise, id: ulid() };
   exercises.push(newExercise);
 };
 
-const updateWorkoutById = (workoutId: number, updatedWorkout: Partial<Workout>) => {
+const updateWorkoutById = (workoutId: string, updatedWorkout: Partial<Workout>) => {
   const workoutIndex = workouts.findIndex((workout) => workout.id === workoutId);
   if (workoutIndex !== -1) {
     workouts[workoutIndex] = { ...workouts[workoutIndex], ...updatedWorkout };
   }
 };
 
-const updateExerciseById = (exerciseId: number, updatedExercise: Partial<Exercise>) => {
+const updateExerciseById = (exerciseId: string, updatedExercise: Partial<Exercise>) => {
   const exerciseIndex = exercises.findIndex((exercise) => exercise.id === exerciseId);
   if (exerciseIndex !== -1) {
     exercises[exerciseIndex] = { ...exercises[exerciseIndex], ...updatedExercise };
   }
 };
 
-const deleteWorkoutById = (workoutId: number) => {
+const deleteWorkoutById = (workoutId: string) => {
   const index = workouts.findIndex((workout) => workout.id === workoutId);
   if (index !== -1) {
     workouts.splice(index, 1);
   }
 };
 
-const deleteExerciseById = (exerciseId: number) => {
+const deleteExerciseById = (exerciseId: string) => {
   const index = exercises.findIndex((exercise) => exercise.id === exerciseId);
   if (index !== -1) {
     exercises.splice(index, 1);
@@ -56,13 +55,13 @@ const deleteExerciseById = (exerciseId: number) => {
 };
 
 export {
-  saveExercise,
-  saveWorkout,
   deleteExerciseById,
   deleteWorkoutById,
   getExercises,
   getExercisesByWorkout,
   getWorkouts,
+  saveExercise,
+  saveWorkout,
   updateExerciseById,
   updateWorkoutById,
 };
