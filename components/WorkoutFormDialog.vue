20a68d3c8fac5993e3a3f892dd7c46bd1c4e1595
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-24 19:41:03 -0300

    Add WorkoutFormDialog component

---
+++ components/WorkoutFormDialog.vue
@@ -0,0 +1,81 @@
+<script lang="ts" setup>
+import { toTypedSchema } from "@vee-validate/zod";
+import { useForm } from "vee-validate";
+import * as z from "zod";
+import type { Workout } from "~/types";
+
+const props = defineProps<{
+  open: boolean;
+  workout?: Workout;
+}>();
+
+const emit = defineEmits(["update:open"]);
+
+const formSchema = toTypedSchema(
+  z.object({
+    name: z.string().min(2, { message: "Name must be at least 2 characters long" }).max(50),
+  }),
+);
+
+const form = useForm({
+  validationSchema: formSchema,
+  initialValues: props.workout || {},
+});
+
+const submit = form.handleSubmit((values) => {
+  console.log("Form submitted!", values);
+});
+
+const toggleDialog = () => {
+  form.resetForm();
+  emit("update:open", !props.open);
+};
+</script>
+
+<template>
+  <Dialog
+    :open="open"
+    @update:open="toggleDialog"
+  >
+    <DialogContent class="sm:max-w-[425px]">
+      <div className="space-y-6">
+        <div>
+          <h3 className="text-lg font-medium">
+            Workout
+          </h3>
+          <p className="text-sm text-muted-foreground">
+            Plan your workout.
+          </p>
+        </div>
+
+        <form
+          class="space-y-6"
+          @submit="submit"
+        >
+          <FormField
+            v-slot="{ componentField }"
+            name="name"
+          >
+            <FormItem>
+              <FormLabel>Nome</FormLabel>
+              <FormControl>
+                <Input
+                  type="text"
+                  placeholder="Treino A"
+                  v-bind="componentField"
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          </FormField>
+
+          <DialogFooter>
+            <Button type="submit">
+              Salvar
+            </Button>
+          </DialogFooter>
+        </form>
+      </div>
+    </DialogContent>
+  </Dialog>
+</template>
