e9d209ea484bfc3e63193e5af28c07790d0c3659
katsuragawaa <andre.katsuragawa@gmail.com>
2024-03-24 21:51:25 -0300

    Add DeleteAlertDialog component

---
+++ components/DeleteAlertDialog.vue
@@ -0,0 +1,27 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  open: boolean;
+}>();
+
+const emit = defineEmits(["update:open"]);
+</script>
+
+<template>
+  <AlertDialog
+    :open="props.open"
+    @update:open="emit('update:open', $event)"
+  >
+    <AlertDialogContent>
+      <AlertDialogHeader>
+        <AlertDialogTitle>Tem certeza?</AlertDialogTitle>
+        <AlertDialogDescription>
+          Essa ação não pode ser desfeita. Todos os dados relacionados serão perdidos.
+        </AlertDialogDescription>
+      </AlertDialogHeader>
+      <AlertDialogFooter>
+        <AlertDialogCancel>Cancelar</AlertDialogCancel>
+        <AlertDialogAction>Continuar</AlertDialogAction>
+      </AlertDialogFooter>
+    </AlertDialogContent>
+  </AlertDialog>
+</template>
